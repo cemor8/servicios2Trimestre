8dadf7dd068401e10201d4af34baff738af5bba6
Carlos Morais Blanco <carlosmoraisblanco@gmail.com>
2024-01-20 21:53:08 +0100

    todo

--- ej4/src/Cliente.java
+++ ej4/src/Cliente.java
@@ -4,52 +4,62 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.PortUnreachableException;
 
-public class Cliente {
+public class Cliente extends Thread{
     //host del server
     private int puertoEnviar = 4321;
     //almacena paquetes
-    byte[] buffer = new byte[25];
+    byte[] buffer = new byte[5];
     InetAddress direccionServer;
     DatagramSocket socketUdp;
     private Integer posicion;
+    private int cantidadClientes;
 
 
-    public Cliente(Integer posicion) throws IOException {
+    public Cliente(Integer posicion,Integer cantidadClientes) throws IOException {
         this.posicion = posicion;
-       direccionServer = InetAddress.getByName("localhost");
-       socketUdp = new DatagramSocket();
+        this.cantidadClientes = cantidadClientes;
+        direccionServer = InetAddress.getByName("localhost");
+        int puertoLocal = puertoEnviar + posicion; // Puerto en el que este cliente escuchará
+        socketUdp = new DatagramSocket(puertoLocal);
 
     }
-    public void  iniciarCliente() throws IOException {
-        if(posicion == 1){
-            System.out.println("Cliente "+ posicion+ ": envia token");
+    @Override
+    public void run(){
+        try {
+            this.iniciarCliente();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void iniciarCliente() throws IOException {
+        int siguientePosicion = (posicion % cantidadClientes) + 1;
+        int puertoDestino = puertoEnviar + siguientePosicion; // Puerto al que este cliente enviará el token
+
+        if (posicion == 1) {
+            // Cliente 1: Envía el token al inicio
+            System.out.println("Cliente " + posicion + ": envía token");
             String mensaje = "token";
             buffer = mensaje.getBytes();
-            DatagramPacket pregunta = new DatagramPacket(buffer, buffer.length,direccionServer,puertoEnviar+posicion);
+            DatagramPacket pregunta = new DatagramPacket(buffer, buffer.length, direccionServer, puertoDestino);
             socketUdp.send(pregunta);
-        }else {
-            System.out.println("Esperando respuesta");
-            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
-            socketUdp.receive(peticion);
-            System.out.println("Cliente "+this.posicion+" recibe token");
-            String mensaje = new String(peticion.getData());
-            System.out.println(mensaje);
-            System.out.println("Cliente "+this.posicion+" recibe token");
-
-            if (this.posicion != 1 || !mensaje.equalsIgnoreCase("token")){
-
-            }
-
-
-
-            socketUdp.close();
         }
 
-        // mandar paquete a server
+        // Todos los clientes: Esperan recibir el token
+        System.out.println("Cliente " + posicion + ": esperando respuesta");
+        DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+        socketUdp.receive(peticion);
+        System.out.println("Cliente " + posicion + ": recibe token");
+        String mensaje = new String(peticion.getData(), 0, peticion.getLength()).trim();
 
-
-        //recibir info
-
-
+        if (posicion != 1 || !mensaje.equalsIgnoreCase("token")) {
+            // Cliente reenvía el token al siguiente cliente en el anillo
+            System.out.println("Cliente: " + posicion + ": reenvía token");
+            peticion.setPort(puertoDestino);
+            socketUdp.send(peticion);
+        } else {
+            // El Cliente 1 recibe el token de vuelta y termina el ciclo
+            System.out.println("Cliente 1 ha recibido el token de vuelta. Terminando el ciclo.");
+        }
+        socketUdp.close();
     }
 }
--- ej4/src/MainCliente.java
+++ ej4/src/MainCliente.java
@@ -1,8 +1,30 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainCliente {
     public static void main(String[] args) throws IOException {
-        Cliente cliente = new Cliente();
-        cliente.iniciarCliente();
+        Integer opcion = 0;
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        while (opcion == 0){
+            Scanner scanner = new Scanner(System.in);
+            try {
+                opcion = scanner.nextInt();
+                if(opcion <=0 ||opcion > 10){
+                    opcion = 0;
+                    continue;
+                }
+            }catch (Exception err){
+                System.out.println(err.getMessage());
+            }
+
+            for (int i = 0; i<opcion;i++){
+                Cliente cliente = new Cliente(i+1,opcion);
+                clientes.add(cliente);
+            }
+            for (Cliente cliente : clientes){
+                cliente.start();
+            }
+        }
     }
 }
